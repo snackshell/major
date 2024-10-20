52ff9040a2c9730a3a8a191def48120bbca0d0f1
paradox <solomonadonay1@gmail.com>
2024-10-20 22:45:38 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -507,6 +507,7 @@ async def main():
 
 if __name__ == "__main__":
     try:
-        asyncio.run(main())
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(main())
     except KeyboardInterrupt:
         exit()
